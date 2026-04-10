b7a593787e9e733b966abfc29ad28aa4d53a35b8
Uttkarsh <uttkarshchambiyal26@gmail.com>
2026-04-10 13:53:34 +0000

    Auto-sync LeetCode solutions Runtime - 1078 ms (32.54%), Memory - 7.8 MB (19.37%)

---
+++ solutions/0507-perfect-number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count = 0;
+        for(int i = 1; i <= num/2; i++){
+            if(num%i==0){
+                count = count + i;
+            }
+        }
+        if(count == num){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
