0a4df968c7a95f6e21ec99c369188e9d4b0fc28e
Uttkarsh <uttkarshchambiyal26@gmail.com>
2026-04-23 19:18:56 +0000

    Auto-sync LeetCode solutions Runtime - 57 ms (72.05%), Memory - 174 MB (47.55%)

---
+++ solutions/4141-count-elements-with-at-least-k-greater-values/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums, int k) {
+       int count = 0;
+       int n = nums.size();
+         if(k == 0) return n;
+sort(nums.begin(),nums.end());
+
+int threshold = nums[n - k];
+
+for(int i = 0; i<n; i++){
+    if(nums[i] < threshold){
+        count++;
+    }
+
+    }
+    return count;
+    }
+};
