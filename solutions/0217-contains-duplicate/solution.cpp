1e7fcae27668c2af609e7ebf4da2aa46f60132e0
Uttkarsh <uttkarshchambiyal26@gmail.com>
2026-05-09 20:39:44 +0000

    Auto-sync LeetCode solutions Runtime - 67 ms (17.03%), Memory - 93.1 MB (8.29%)

---
+++ solutions/0217-contains-duplicate/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> unique;
+        for(int i = 0; i < nums.size(); i++){
+            unique.insert(nums[i]);
+        }
+        if(unique.size() == nums.size()){
+              return false;
+        }
+        else{
+      
+              return true;
+        }
+    }
+};
