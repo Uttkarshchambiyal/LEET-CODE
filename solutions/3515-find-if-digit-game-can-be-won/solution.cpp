55e0640ca5bb09cb7f67e980742012255805bc1a
Uttkarsh <uttkarshchambiyal26@gmail.com>
2026-05-19 09:09:28 +0000

    Auto-sync LeetCode solutions Runtime - 0 ms (100.00%), Memory - 28.8 MB (11.41%)

---
+++ solutions/3515-find-if-digit-game-can-be-won/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int sum = 0;
+        int sum2 = 0;
+        int n = nums.size();
+         for(int i = 0; i < n; i++){
+            if(nums[i]<=9){
+            sum =sum+nums[i];
+            }
+            if(nums[i]>9){
+                sum2 = sum2 + nums[i];
+            }
+
+         }
+         if(sum==sum2){
+            return false;
+         }
+         else{
+            return true;
+         }
+    }
+};
